ec7fbbe3722cf1b3d3247cad7c0d78038ebd1001
Ant Kennedy <antjkennedy@gmail.com>
2014-08-24 18:07:28 +0100

    minor changes to the fish_prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -73,5 +73,11 @@ function fish_prompt --description 'Write out the prompt'
 		set -g __fish_prompt_host (set_color $fish_color_host)
 	end
 
-	echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+  if not set -q __fish_prompt_git
+    set -g __fish_prompt_git (set_color 9b59b6)
+  end
+
+	echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) 
+  echo -n -s (set_color 9b59b6) (__fish_git_prompt) 
+  echo -e -n -s '\n' "$__fish_prompt_normal" "$delim" ' '
 end
