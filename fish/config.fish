bb7a10a05abfbb0ee87f97e5ccf08dc80bc52225
Ant Kennedy <anthony.j.kennedy@boeing.com>
2014-08-29 09:42:38 +0100

    updated config.fish to add a few environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,14 @@
-set fish_greeting ""
+# clear the greeting
+set -g -x fish_greeting ''
+
+# Environment variables 
+
+prepend_to_path "/usr/local/sbin"
+prepend_to_path "/usr/local/bin"
+prepend_to_path "$HOME/.local/bin"
+deduplicate PATH                                   # Clear duplicates in path
+
+set -x ARCHFLAGS "-arch x86_64"
+set -x EDITOR vim
+set -x LC_ALL en_GB.UTF-8
+
